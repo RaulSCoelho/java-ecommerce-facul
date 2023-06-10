274790168a9dea40fe184da0e9f027be64c6c30f
RaulSCoelho <84609153+RaulSCoelho@users.noreply.github.com>
2023-06-10 03:35:24 -0300

    startProgram

--- src/main/java/com/ecommerce/App.java
+++ src/main/java/com/ecommerce/App.java
@@ -2,13 +2,45 @@ package com.ecommerce;
 
 import com.ecommerce.controller.UserController;
 import com.ecommerce.models.User;
+import com.ecommerce.utils.ScannerUtils;
 
 public class App {
     private static UserController userController = new UserController();
+    private static User user;
 
     public static void main(String[] args) {
-        User user = userController.login("RaulSCoelho", "teste123");
-        if (user != null)
-            System.out.println(user.toString());
+        startProgram();
+    }
+
+    public static void startProgram() {
+        if (user == null) {
+            System.out.println("Escolha uma ação: ");
+            System.out.println("1 - Login");
+            System.out.println("0 - Finalizar o programa");
+        } else {
+            System.out.println("Escolha uma ação: ");
+            System.out.println("1 - Logout");
+            System.out.println("0 - Finalizar programa");
+        }
+
+        int action = ScannerUtils.nextInt();
+
+        if (action == 0)
+            return;
+
+        System.out.println();
+
+        try {
+            switch (action) {
+                case 1:
+                    userController.login(ScannerUtils.nextLine("Username: "), ScannerUtils.nextLine("Password: "));
+                    break;
+            }
+        } catch (Throwable ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        System.out.println();
+        startProgram();
     }
 }
